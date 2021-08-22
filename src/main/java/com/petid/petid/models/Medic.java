70f3cdf3ca905bb2be13a1d71d78d4dcfb283833
PGi-x2 <85518940+PGi-x2@users.noreply.github.com>
2021-08-22 22:44:27 +0300

    Medic class added

---
+++ src/main/java/com/petid/petid/models/Medic.java
@@ -0,0 +1,24 @@
+package com.petid.petid.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Table
+@Entity
+
+public class Medic {
+    @Id
+    private int id;
+    @NotNull
+    private int personId;
+    @NotNull
+    private String userId;
+    @NotNull
+    private String stampNumber;
+    @NotNull
+    private List<Practice> practices;
+
+}
